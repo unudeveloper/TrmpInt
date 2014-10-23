santam85/angular-gantt

refactor(columns): Use momentjs for column handling
This is a big refactor in column handling using momentJS features.

view-scale now supports any of momentJS unit, like year, quarter and minute.

headers-* options have been dropped, in favor of headers and headersFormats (see README).

This drops support of everything related to working hours, weekends and column-sub-scale. Those options will be replaced by #126 and #123.

Close #79

src/logic/headerGenerator.factory.js
@@ -1,90 +1,35 @@
 'use strict';
-gantt.factory('HeaderGenerator', [ 'Column', 'moment', function(Column, moment) {
-
-    var generateHourHeader = function(columns) {
+gantt.factory('HeaderGenerator', [ 'Column', function(Column) {
+    var generateHeader = function(columns, unit) {
         var generatedHeaders = [];
-
         var header;
         for (var i = 0, l = columns.length; i < l; i++) {
             var col = columns[i];
-            if (i === 0 || columns[i - 1].date.hour() !== col.date.hour()) {
-                header = new Column.Hour(moment(col.date), col.left, col.width, col.isWeekend, col.isWorkHour);
+            if (i === 0 || columns[i - 1].date.get(unit) !== col.date.get(unit)) {
+                header = new Column(col.date, unit, col.left, col.width);
                 generatedHeaders.push(header);
             } else {
                 header.width += col.width;
             }
         }
-
-        return generatedHeaders;
-    };
-
-    var generateDayHeader = function(columns) {
-        var generatedHeaders = [];
-
-        var header;
-        for (var i = 0, l = columns.length; i < l; i++) {
-            var col = columns[i];
-            if (i === 0 || columns[i - 1].date.day() !== col.date.day()) {
-                header = new Column.Day(moment(col.date), col.left, col.width, col.isWeekend, col.daysToNextWorkingDay, col.daysToPrevWorkingDay);
-                generatedHeaders.push(header);
-            } else {
-                header.width += col.width;
-            }
-        }
-
-        return generatedHeaders;
-    };
-
-    var generateWeekHeader = function(columns) {
-        var generatedHeaders = [];
-
-        var header;
-        for (var i = 0, l = columns.length; i < l; i++) {
-            var col = columns[i];
-            if (i === 0 || columns[i - 1].date.week() !== col.date.week()) {
-                header = new Column.Week(moment(col.date), col.left, col.width, col.date.week());
-                generatedHeaders.push(header);
-            } else {
-                header.width += col.width;
-            }
-        }
-
-        return generatedHeaders;
-    };
-
-    var generateMonthHeader = function(columns) {
-        var generatedHeaders = [];
-
-        var header;
-        for (var i = 0, l = columns.length; i < l; i++) {
-            var col = columns[i];
-            if (i === 0 || columns[i - 1].date.month() !== col.date.month()) {
-                header = new Column.Month(moment(col.date), col.left, col.width);
-                generatedHeaders.push(header);
-            } else {
-                header.width += col.width;
-            }
-        }
-
         return generatedHeaders;
     };
 
     return {
         instance: function($scope) {
             this.generate = function(columns) {
-                var headers = {};
-                if ($scope.headerShowHour && ['hour'].indexOf($scope.viewScale) > -1) {
-                    headers.hour = generateHourHeader(columns);
+                var units = [];
+                if ($scope.headers === undefined) {
+                    units = [$scope.viewScale];
+                } else {
+                    units = $scope.headers;
                 }
-                if ($scope.headerShowDay && ['hour', 'day'].indexOf($scope.viewScale) > -1) {
-                    headers.day = generateDayHeader(columns);
-                }
-                if ($scope.headerShowWeek && ['week'].indexOf($scope.viewScale) > -1) {
-                    headers.week = generateWeekHeader(columns);
-                }
-                if ($scope.headerShowMonth && ['week', 'month'].indexOf($scope.viewScale) > -1) {
-                    headers.month = generateMonthHeader(columns);
-                }
+
+                var headers = [];
+                angular.forEach(units, function(unit) {
+                    headers.push(generateHeader(columns, unit));
+                });
+
                 return headers;
             };
         }

unudeveloper/TrmpInt

src/logic/headerGenerator.factory.js
@@ -1,90 +1,35 @@
 'use strict';
-gantt.factory('GanttHeaderGenerator', [ 'GanttColumn', 'moment', function(Column, moment) {
-
-    var generateHourHeader = function(columns) {
+gantt.factory('GanttHeaderGenerator', [ 'GanttColumn', function(Column) {
+    var generateHeader = function(columns, unit) {
         var generatedHeaders = [];
-
         var header;
         for (var i = 0, l = columns.length; i < l; i++) {
             var col = columns[i];
-            if (i === 0 || columns[i - 1].date.hour() !== col.date.hour()) {
-                header = new Column.Hour(moment(col.date), col.left, col.width, col.isWeekend, col.isWorkHour);
+            if (i === 0 || columns[i - 1].date.get(unit) !== col.date.get(unit)) {
+                header = new Column(col.date, unit, col.left, col.width);
                 generatedHeaders.push(header);
             } else {
                 header.width += col.width;
             }
         }
-
-        return generatedHeaders;
-    };
-
-    var generateDayHeader = function(columns) {
-        var generatedHeaders = [];
-
-        var header;
-        for (var i = 0, l = columns.length; i < l; i++) {
-            var col = columns[i];
-            if (i === 0 || columns[i - 1].date.day() !== col.date.day()) {
-                header = new Column.Day(moment(col.date), col.left, col.width, col.isWeekend, col.daysToNextWorkingDay, col.daysToPrevWorkingDay);
-                generatedHeaders.push(header);
-            } else {
-                header.width += col.width;
-            }
-        }
-
-        return generatedHeaders;
-    };
-
-    var generateWeekHeader = function(columns) {
-        var generatedHeaders = [];
-
-        var header;
-        for (var i = 0, l = columns.length; i < l; i++) {
-            var col = columns[i];
-            if (i === 0 || columns[i - 1].date.week() !== col.date.week()) {
-                header = new Column.Week(moment(col.date), col.left, col.width, col.date.week());
-                generatedHeaders.push(header);
-            } else {
-                header.width += col.width;
-            }
-        }
-
-        return generatedHeaders;
-    };
-
-    var generateMonthHeader = function(columns) {
-        var generatedHeaders = [];
-
-        var header;
-        for (var i = 0, l = columns.length; i < l; i++) {
-            var col = columns[i];
-            if (i === 0 || columns[i - 1].date.month() !== col.date.month()) {
-                header = new Column.Month(moment(col.date), col.left, col.width);
-                generatedHeaders.push(header);
-            } else {
-                header.width += col.width;
-            }
-        }
-
         return generatedHeaders;
     };
 
     return {
         instance: function($scope) {
             this.generate = function(columns) {
-                var headers = {};
-                if ($scope.headerShowHour && ['hour'].indexOf($scope.viewScale) > -1) {
-                    headers.hour = generateHourHeader(columns);
+                var units = [];
+                if ($scope.headers === undefined) {
+                    units = [$scope.viewScale];
+                } else {
+                    units = $scope.headers;
                 }
-                if ($scope.headerShowDay && ['hour', 'day'].indexOf($scope.viewScale) > -1) {
-                    headers.day = generateDayHeader(columns);
-                }
-                if ($scope.headerShowWeek && ['day', 'week'].indexOf($scope.viewScale) > -1) {
-                    headers.week = generateWeekHeader(columns);
-                }
-                if ($scope.headerShowMonth && ['day', 'week', 'month'].indexOf($scope.viewScale) > -1) {
-                    headers.month = generateMonthHeader(columns);
-                }
+
+                var headers = [];
+                angular.forEach(units, function(unit) {
+                    headers.push(generateHeader(columns, unit));
+                });
+
                 return headers;
             };
         }